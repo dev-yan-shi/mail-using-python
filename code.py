63fffd24ddbffc44be85ced6d3b910f016a89b01
dev-yan-shi <49531745+dev-yan-shi@users.noreply.github.com>
2019-04-11 23:53:45 +0530

    Update code.py

--- code.py
+++ code.py
@@ -2,6 +2,6 @@ import smtplib
 s = smtplib.SMTP('smtp.gmail.com',587)
 s.starttls()
 s.login('devyanshilko@gmail.com','password')
-content = ''
+content = 'Hi! last minute submission , humour quotient =zero:) P.S: please take me in, just kidding(no i am not)'
 s.sendmail('devyanshilko@gmail.com','kushgpt99@gmail.com', content)
 s.quit()
